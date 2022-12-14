5c54232f0899deab2c8386ba3cabe84d89048f23
Sam Brown <samsterbrownie@gmail.com>
2022-12-14 12:38:28 +0000

    convert cortex to stm32

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,15 @@
 #![no_main]
 
 // pick a panicking behavior
-use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+use cortex_m_rt::{exception, entry};
 
-use cortex_m::asm;
-use cortex_m_rt::entry;
+use panic_semihosting as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+use stm32f3xx_hal::{self as hal, pac as pac, prelude::*};
+use cortex_m_semihosting::hprintln;
 
 #[entry]
 fn main() -> ! {
-    asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
-
-    loop {
-        // your code goes here
-    }
+    let mut p = pac::Peripherals::take().unwrap();
+    let mut  cp = pac::CorePeripherals::take().unwrap();
+    loop{}
 }
