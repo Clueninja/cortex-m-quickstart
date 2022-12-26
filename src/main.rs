29d3f6fc3274e93ba732601ddd643f9a0795622c
Sam Brown <samsterbrownie@gmail.com>
2022-12-26 19:04:18 +0000

    Add more code to the main function to speed up programming

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,25 @@
 #![no_std]
 #![no_main]
 
-// pick a panicking behavior
-use cortex_m_rt::{exception, entry};
 
-use panic_semihosting as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-use stm32f3xx_hal::{self as hal, pac as pac, prelude::*};
-use cortex_m_semihosting::hprintln;
+use cortex_m_rt::{exception, entry};
+// use panic_halt as _;
+use panic_semihosting as _;
+use stm32f3xx_hal::{self as hal, pac, prelude::*};
+use cortex_m_semihosting::{hprintln, hprint};
 
 #[entry]
 fn main() -> ! {
-    let mut p = pac::Peripherals::take().unwrap();
-    let mut  cp = pac::CorePeripherals::take().unwrap();
+    let p = pac::Peripherals::take().unwrap();
+    let _cp = pac::CorePeripherals::take().unwrap();
+    let mut rcc = p.RCC.constrain();
+    let mut flash = p.FLASH.constrain();
+    let clocks = rcc.cfgr.freeze(&mut flash.acr);
+
+    let _gpioa = p.GPIOA.split(&mut rcc.ahb);
+    let _gpiob = p.GPIOB.split(&mut rcc.ahb);
+    let _gpioc = p.GPIOC.split(&mut rcc.ahb);
+    let _gpiod = p.GPIOD.split(&mut rcc.ahb);
+    let _gpioe = p.GPIOE.split(&mut rcc.ahb);
     loop{}
 }
